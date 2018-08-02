073c6993e6b53f8aed7f9ad181d3784e3d5fe93b
Marcin <bomarc2@gmail.com>
2018-08-02 22:18:14 +0200

    LoginTemplate

--- web-ui/src/app/login/login.component.ts
+++ web-ui/src/app/login/login.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  loginData: any = {};
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  login(form)
+  {
+    console.log(form.value);
+    
+  }
 }

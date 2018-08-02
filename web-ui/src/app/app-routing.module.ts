2428848d45271082b31816e5ca5e5f6eb163f95a
Marcin <bomarc2@gmail.com>
2018-08-02 19:49:34 +0200

    SetupFix and RoutingSetup

---
+++ web-ui/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+]
+
+@NgModule({
+  exports: [
+    RouterModule
+  ],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+})
+export class AppRoutingModule { }
--- web-ui/src/app/app.module.ts
+++ web-ui/src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '../../node_modules/@angular/forms';
+import { AppRoutingModule } from './/app-routing.module';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot(),
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]

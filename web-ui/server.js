f086c7db8731b9fd80e5f0b5aa6c7902d7fa0826
Marcin <bomarc2@gmail.com>
2018-08-25 13:07:47 +0200

    proxySetup

--- web-ui/server.js
+++ web-ui/server.js
@@ -1,16 +1,23 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const proxy = require('express-http-proxy');
 
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/web-ui'));
+app.use('/api', proxy(''));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname,'/dist/web-ui/index.html'));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, '/dist/web-ui/index.html'));
 });
 
+const port = process.env.PORT || '8080';
+app.set('port', port);
+
+const server = http.createServer(app);
+
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+server.listen(port, () => console.log(`Running`));

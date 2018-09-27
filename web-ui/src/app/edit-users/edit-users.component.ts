94f7522bc755a9c70564e31d9b6e453fcb5ddf31
Marcin <bomarc2@gmail.com>
2018-09-27 07:45:31 +0200

    4,5Fix

--- web-ui/src/app/edit-users/edit-users.component.ts
+++ web-ui/src/app/edit-users/edit-users.component.ts
@@ -10,6 +10,11 @@ import { DatabaseService } from '../_service/database.service';
 export class EditUsersComponent implements OnInit {
 
   users
+  userTypes = [
+    {type: 'pacjent', value: 0},
+    {type: 'lekarz', value: 1},
+    {type: 'admin', value: 2}
+  ]
 
   constructor(
     private authService: AuthService,
